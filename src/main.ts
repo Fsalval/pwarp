691ff52302b6720626c7de390d4d086f8a08b417
Fer <fernanda.salinas.val@gmail.com>
2026-06-24 08:28:07 -0400

    modal pem

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import './style.css'
 import { initEditor } from './ui/editor'
+import { initLegalModal } from './lib/LegalModal'
 
+
+initLegalModal(supabase)  // tu cliente existente
+
+initLegalModal(supabase)  // tu cliente existente
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 initEditor(app)
